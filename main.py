e84a6a549d6127a93b70620b188b69ff78303c11
Jacob Trentini <jacobtrentinidevelopment@gmail.com>
2022-01-07 00:26:14 +0000

    Added different letters and vowels count

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # main.py
+from collections import Counter
+
+sentence = input("Enter a sentence: ").replace(" ", "")
+count = Counter(sentence)
+
+# different letters
+
+print(len(count))
+
+# vowels
+print(count["a"] + count["e"] + count["i"] + count["o"] + count["u"])
